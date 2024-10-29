619da269f51d79242335c3f5eeb19335c15bf618
candbright <candbright@outlook.com>
2024-10-29 23:55:04 +0800

    temp

---
+++ src/components/ServerInfo.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "ServerInfo",
+  props: {
+    data: {
+      type: Array
+    },
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+<template>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content ep-bg-purple-dark" />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Server from '../components/ServerInfo.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Server />
   </main>
 </template>
